206ec4a95274291b0885b9ef572c966ea79daef5
Fernando Cusco <cuzcator@gmail.com>
2019-07-30 01:04:53 -0500

    -manejo de diccionarios

--- Manejo.py
+++ Manejo.py
@@ -8,4 +8,13 @@ cursos = ["octavo", "noveno", "decimo"]
 
 
 print(nombres)
+print(cursos)
+
+#diccionarios
+#diccionario con un tipo de dato
+mi_diccionario = {'ala':'parte de una ave', 'numero':'1234'}
+
+
+
+print(mi_diccionario.keys())
 
