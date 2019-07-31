62e6ffc9bcdecd3129b1c10aeb1b86f5866a0849
Fernando Cusco <cuzcator@gmail.com>
2019-07-30 23:55:32 -0500

    manejo de diccionarios

--- Manejo.py
+++ Manejo.py
@@ -3,9 +3,18 @@ print("Hola")
 #arreglo
 nombres = ["Fernando", "Ana", "Maria", "Jose"]
 
+print('Soy un arreglo: ',nombres)
 
 cursos = ["octavo", "noveno", "decimo"]
 
+#diccinarios
+#diccionario con mismo tipo de dato
+diccionario = {'clave':'valor', 'triangulo':'figura geometrica de 3 lados'}
 
-print(nombres)
+print('Soy un diccionario: ', diccionario)
+
+#get obtenemos el valor dentro del diccionario pasando como parametro su clave
+print(diccionario.get('triangulo'))
+
+
 
