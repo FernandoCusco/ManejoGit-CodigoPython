8e6e14726c70d2a299d2fd11f8f09c73e0fc870d
Fernando Cusco <cuzcator@gmail.com>
2019-08-01 11:39:27 -0500

    sentencias de control if-while

--- Manejo.py
+++ Manejo.py
@@ -21,3 +21,40 @@ print(diccionario.get('triangulo'))
 diccionario1 = {'clave':'valor', 'nombre':'Fernando', 'apellido':'Cusco', 'edad':23, 'peso':110.2, 'estado':False}
 print(diccionario1.keys())
 print(diccionario1.values())
+
+
+
+#sentencias de control
+
+#if
+edad = 45
+peso = 10
+nivelMedicina = "Cantidad sugerida 50%"
+
+if edad > 18 and peso < 100:
+	nivelMedicina = "Cantidad sugerida 25%"
+
+else:
+	if edad < 18 and peso < 65:
+		nivelMedicina = "Cantidad sugerida 10-15%"
+
+print(nivelMedicina)
+
+
+#ciclos
+
+contador = 0
+tm = 0
+while(contador < 11):
+	tm = 5 * contador
+	print('5 * ',contador, '=',tm)
+	contador+=1
+
+
+
+
+
+
+
+
+
